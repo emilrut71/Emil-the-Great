6cdc0d01ac0b1a9d9092ff91b26865dd9db47c5a
emilrut71 <55450100+emilrut71@users.noreply.github.com>
2021-04-01 15:55:21 +0200

    Create storage.js

---
+++ storage.js
@@ -0,0 +1,10 @@
+let element = document.getElementById('Förnamn');
+let andraelement = document.getElementById('Efternamn');
+
+function storage(){
+  localStorage.setItem('element1', element.value);
+  localStorage.setItem('element2', andraelement.value);
+}
+function check(){
+    
+}
