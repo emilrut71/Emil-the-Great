99bc9526af141969d10a19f44ab4a0ef78ee0196
emilrut71 <55450100+emilrut71@users.noreply.github.com>
2021-05-27 15:21:23 +0200

    Create tictac.js

---
+++ tictac.js
@@ -0,0 +1,15 @@
+var board = [
+    ["","",""],
+    ["","",""],
+    ["","",""],
+]
+let spelar1 = "X";
+let spelar2 = "O";
+
+function hej(){
+    let a = "X";
+}
+
+function hej() {
+    document.getElementById("1").innerHTML = "X";
+  }
